6a234179fe8e3541f359566afeb5c08409d5eded
Andrew Li <lte1160106854@gmail.com>
2025-10-30 03:02:18 -0400

    Implemented PAXOS phase 1

--- comp512p2/comp512st/paxos/Paxos.java
+++ comp512p2/comp512st/paxos/Paxos.java
@@ -9,7 +9,9 @@ import comp512.utils.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.logging.*;
@@ -22,9 +24,8 @@ import java.net.UnknownHostException;
 //		You should also not change the signature of these methods (arguments and return value) other aspects maybe changed with reasonable design needs.
 public class Paxos
 {
-    List<String> allGroupProcesses;
+    List<String> allGroupProcesses = new ArrayList<String>();
     
-
 	GCL gcl;
 	FailCheck failCheck;
 
@@ -33,7 +34,14 @@ public class Paxos
 
     int promisedBID = -1;
     int proposedBID = -1;
-    String acceptedVal = null;
+    Object proposedVal;
+    Object originalProposedVal;
+
+    int acceptedBID = -1;
+    Object acceptedVal = null;
+
+    Map<Integer, Object> prevAcceptedIDVals = new HashMap<>();
+    List<String> promisedProcess = new ArrayList<String>();
 
 	public Paxos(String myProcess, String[] allGroupProcesses, Logger logger, FailCheck failCheck) throws IOException, UnknownHostException
 	{
@@ -77,8 +85,8 @@ public class Paxos
         List<String> validTypes = Arrays.asList("PROPOSE", "PROMISE", "REFUSE", "ACCEPT", "ACCEPTED", "DECIDE");
 
         int ballotID;
-        int acceptedBID;
-        String acceptedVal;
+        int prevAcceptedBID = -1;
+        Object prevAcceptedVal;
 
         public PaxosMessage(String type){
             if (validTypes.contains(type.toUpperCase())){
@@ -94,12 +102,12 @@ public class Paxos
             this.ballotID = ballotID;
         }
 
-        public void setAcceptedBID(int acceptedBID) {
-            this.acceptedBID = acceptedBID;
+        public void setPrevAcceptedBID(int prevAcceptedBID) {
+            this.prevAcceptedBID = prevAcceptedBID;
         }
 
-        public void setAcceptedVal(String acceptedVal) {
-            this.acceptedVal = acceptedVal;
+        public void setPrevAcceptedVal(Object prevAcceptedVal) {
+            this.prevAcceptedVal = prevAcceptedVal;
         }
 
     }
@@ -112,25 +120,55 @@ public class Paxos
         gcl.broadcastMsg(msg);
     }
 
-    private boolean hasMajorityPromised(int ballotID, String process){
-        List<String> promisedProcess = new ArrayList<String> ();
-        
-        if () {
-            
+    private boolean hasMajorityPromised(){
+        long start = System.currentTimeMillis();
+        // allow 1 second to hear back the promises
+        long timeout = 1000;
+        while (System.currentTimeMillis() - start < timeout) {
+            // return true as soon as reach the majority
+            if (promisedProcess.size() > allGroupProcesses.size()/2) {
+                return true;
+            }
         }
+        // if time out
+        return false;
     }
 
     public void broadcastTOMsg_temp(Object val) {
         ballotIDCounter ++;
         int ballotID = ballotIDCounter;
         proposedBID = ballotID;
+        proposedVal = val;
         proposeToAll(ballotID);
         
+        if ( !hasMajorityPromised() ){
+            // start over
+            prevAcceptedIDVals = new HashMap<>();
+            promisedProcess = new ArrayList<String>();
+            broadcastTOMsg_temp(val);
+        }
+        else{
+            // if some processes has previously accepted other values
+            if(prevAcceptedIDVals.size()>0){
+                // get the previously accepted value with the highest ballot ID
+                Integer highestID = -1;
+                for (Integer id : prevAcceptedIDVals.keySet()) {
+                    if (id > highestID) {
+                        highestID = id;
+                    }
+                }
+                // propose the value with the highest ballot ID instead, for the rest of the PAXOS round
+                originalProposedVal = proposedVal;      // save the original proposed value
+                proposedVal = prevAcceptedIDVals.get(highestID);
+            }
+        }
 
 
     }
 
     public Object acceptTOMsg_temp() throws InterruptedException {
+        Object confirmedVal = null;
+
         // keeps listening for messages
         while(true){
             GCMessage gcmsg = gcl.readGCMessage();
@@ -147,9 +185,9 @@ public class Paxos
                 else{
                     promiseMsg = new PaxosMessage("PROMISE");
                     // include the previously accepted ballot id and value in the return message
-                    if(acceptedVal != null){
-                        promiseMsg.setAcceptedBID(promisedBID);
-                        promiseMsg.setAcceptedVal(acceptedVal);
+                    if(acceptedBID != -1 && acceptedVal != null){
+                        promiseMsg.setPrevAcceptedBID(acceptedBID);
+                        promiseMsg.setPrevAcceptedVal(acceptedVal);
                     }
                     // promise to the new bollot id
                     promisedBID = poposedBID;
@@ -161,15 +199,22 @@ public class Paxos
             else if (msg.type.equals("PROMISE")) {
                 int promisedBID = msg.ballotID;
                 if(promisedBID == proposedBID){
-                    if (msg.acceptedBID) {
-
+                    promisedProcess.add(sender);
+                    // if previously accepted other value
+                    if (msg.prevAcceptedBID != -1 && msg.prevAcceptedVal != null) {
+                        prevAcceptedIDVals.put(msg.prevAcceptedBID, msg.prevAcceptedVal);
                     }
                 }
 
             }
 
+            else if (msg.type.equals("CONFIRM")) {
+                confirmedVal = null;
+                break;
+            }
         }
-        return gcmsg.val;
+
+        return confirmedVal;
     }
 }
 
